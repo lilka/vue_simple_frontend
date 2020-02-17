860b096f7045b8f780af5bee73b6d08f8316ad6d
lilka <lili.lomnicka@gmail.com>
2020-02-17 19:00:23 +0100

    Displaying from data from backend is working

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -5,73 +5,54 @@
     :items="users"
     :items-per-page="5"
     class = "elevation-1">
+
   </v-data-table>
-</template>
+ </template>
+
 
 <script>
 import axios from 'axios';
 
-export default{
-  data () {
-    return {
-     
-      headers: [
-        {
-        text: 'Users',
-        align: 'left', 
-        sortable: false,
-        value: 'name'
-        }, 
-        {text: 'Surname', value: 'surname'},
-        {text: 'Age', value: 'age'}
-      ],
-      users:[{
-        name: 'Lilla',
-        surname: 'Lilka', 
-        age: 22
-      },{
-        name: 'Dorota',
-        surname: 'Doris', 
-        age: 42
-      },
-      {
-        name: 'Ola',
-        surname: 'Olekx', 
-        age: 44
-      },
-      {
-        name: 'Jana',
-        surname: 'Janeczka', 
-        age: 54
-      },
-        {
-        name: 'Patrycja',
-        surname: 'Pati', 
-        age: 34
-      },
-      {
-        name: 'Michał',
-        surname: 'Mis', 
-        age: 34
-      },
-      ],
-    }
-  },
-  methods: {
-    showUsers(user){
-      axios
-          .get('localhost:5000', {
-            params:{
-
-            }
-          })
-          .then(response => {
-            response = response.data;
-            this
-          })
-    }
+export default {
+ data() {
+  return {
+   dialog: false,
+   headers: [
+    {
+     text: 'Name',
+     align: 'left',
+     sortable: true,
+     value: 'name'
+    },
+    { text: 'Surname', value: 'surname' },
+    { text: 'Age', value: 'age' },
+    { text: 'Actions', value: 'action', sortable: false },
+   ],
+   users: []
   }
-}
+ },
+
+ methods: {
+  showUsers() {
+   const path = 'http://localhost:8888/api/user/';
+   axios.get(path)
+           .then((res) => {
+            console.log(res.data)
+            this.users = res.data;
+            console.log(this.users)
+
+           })
+           .catch((error) => {
+            // eslint-disable-next-line
+            console.error(error);
+           });
+  },
+ },
+
+ created() {
+  this.showUsers();
+ },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
